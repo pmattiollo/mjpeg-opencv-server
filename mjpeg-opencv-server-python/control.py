d0fc869b8fb50498e74d04d4b0427951c2536f1a
AmirRaptoR <amirreza.moghassemi@gmail.com>
2022-11-18 14:16:54 +0100

    ...

--- mjpeg-opencv-server-python/control.py
+++ mjpeg-opencv-server-python/control.py
@@ -1,3 +1,5 @@
+from threading import Thread
+import time
 import requests
 
 
@@ -5,6 +7,9 @@ class Control:
 
     def __init__(self, server_url):
         self.server_url = server_url
+        self.command = None
+        self.command_thread = Thread(target=self.send_command)
+        self.command_thread.start()
 
     def get_sensor_data(self):
         resp = requests.get(url=f'{self.server_url}/sensors')
@@ -16,7 +21,17 @@ class Control:
                 'front_right': fr,
                 'rear_right': rr}
 
-        resp = requests.post(url=f'{self.server_url}/motor', json=data)
-        if resp.status_code != 204:
-            print(resp.text)
+        self.command = ('motor',data)
 
+
+    def send_command(self):
+        while True:
+            try:
+                if self.command is not None:
+                    resp = requests.post(url=f'{self.server_url}/{self.command[0]}', json=self.command[1])
+                    if resp.status_code != 204:
+                        print(resp.text)
+                    self.command = None
+            except:
+                pass
+            time.sleep(0.1)
